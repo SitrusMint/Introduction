53964d1d37b898f9b847df8baa986b0365a7233f
ChocoMint <chocomnt242@gmail.com>
2020-07-26 07:12:49 +0900

    一通りの完成

--- index.js
+++ index.js
@@ -3,5 +3,46 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
     const width = 960;
     const height = 540;
-}
 
+    //レンダラーの作成（雛形？）
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector("#myCanvas")
+    });
+    
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    //シーンの作成（場所の作成）
+    const scene = new THREE.Scene();
+
+    //カメラの作成
+    const camera = new THREE.PerpectiveCamera(
+        45,
+        width/height,
+        1,
+        10000
+    );
+
+    //立方体のジオメトリの作成(形？)
+    const geometry = new THREE.BoxGeometry(500, 500, 500);
+
+    //立方体のマテリアル作成(質感)
+    const material = new THREE.MeshStandardMaterial({
+        color: 0x0000ff
+    });
+
+    //立方体の作成
+    const box = new THREE.Mesh(geometry, material);
+    //そして追加
+    scene.add(box);
+
+    //ライトの作成
+    const light = new THREE.DirectionalLight(0xffffff);
+    light.intensity = 2;    // 光の強さが二倍
+    scene.add(light);
+    //ライトの位置変更
+    light.position.set(1, 1, 1);
+
+    //ついに描画
+    renderer.render(scene, camera);
+}
